eb354fdafaa4665daa3b11e82e4f254809bcb078
Yiwen Liu <gracethedeveloper@gmail.com>
2020-06-08 01:10:24 -0400

    final

--- lab3/scripts/groceries.js
+++ lab3/scripts/groceries.js
@@ -6,7 +6,7 @@ var products = [
         vegetarian: true,
 		glutenFree: true,
 		organic: true,
-		foodGroup: [1,0,0,0],
+		category: "vegetables",
 		image: "https://upload.wikimedia.org/wikipedia/commons/3/33/Brocoli_03.jpg",
 		price: 1.99
     },
@@ -15,7 +15,7 @@ var products = [
 		vegetarian: true,
 		glutenFree: false,
 		organic: false,
-		foodGroup: [0,1,0,0],
+		category: "bakery",
 		image:"https://upload.wikimedia.org/wikipedia/commons/3/3b/Home_made_sour_dough_bread.jpg",
 		price: 1.69
     },
@@ -24,7 +24,7 @@ var products = [
 		vegetarian: false,
 		glutenFree: true,
 		organic: true,
-		foodGroup: [0,0,1,0],
+		category: "meat",
 		image: "https://upload.wikimedia.org/wikipedia/commons/3/36/Steak_03_bg_040306.jpg",
 		price: 8.99
 	},
@@ -33,7 +33,7 @@ var products = [
 		vegetarian: false,
 		glutenFree: true,
 		organic: false,
-		foodGroup: [0,0,1,0],
+		category: "meat",
 		image: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Jumbo_Roasted_pork%2C_Oude_Pekela_%282019%29_01.jpg",
 		price: 12.99
     },
@@ -42,7 +42,7 @@ var products = [
 		vegetarian: true,
 		glutenFree: false,
 		organic: false,
-		foodGroup: [0,1,0,0],
+		category: "bakery",
 		image: "https://upload.wikimedia.org/wikipedia/commons/3/32/Dry_tagliatelle_pasta.jpg",
 		price: 0.99
     },
@@ -51,7 +51,7 @@ var products = [
 		vegetarian: true,
 		glutenFree: true,
 		organic: false,
-		foodGroup: [1,0,0,0],
+		category: "vegetables",
 		image: "https://upload.wikimedia.org/wikipedia/commons/9/94/Salad_platter.jpg",
 		price: 6.99
     },
@@ -60,7 +60,7 @@ var products = [
 		vegetarian: true,
 		glutenFree: true,
 		organic: true,
-		foodGroup: [1,0,0,0],
+		category: "fruits",
 		image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Organic_banana_%283077175990%29.jpg",
 		price: 3.99
 	},
@@ -69,7 +69,7 @@ var products = [
 		vegetarian: true,
 		glutenFree: true,
 		organic: false,
-		foodGroup: [1,0,0,0],
+		category: "fruits",
 		image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Ripening_banana.jpg",
 		price: 2.99
     },
@@ -78,7 +78,7 @@ var products = [
 		vegetarian: true,
 		glutenFree: true,
 		organic: true,
-		foodGroup: [1,0,0,0],
+		category: "fruits",
 		image: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Malus-Boskoop_organic.jpg",
 		price: 4.99
     },
@@ -87,7 +87,7 @@ var products = [
 		vegetarian: false,
 		glutenFree: false,
 		organic: false,
-		foodGroup: [0,0,1,0],
+		category: "meat",
 		image: "https://upload.wikimedia.org/wikipedia/commons/0/06/Burger-King-Chicken-Tenders.jpg",
 		price: 8.99
     },
@@ -96,105 +96,134 @@ var products = [
 		vegetarian: true,
 		glutenFree: true,
 		organic: false,
-		foodGroup: [1,0,0,0],
+		category: "fruits",
 		image: "https://upload.wikimedia.org/wikipedia/commons/4/43/Ambersweet_oranges.jpg",
 		price: 7.99
-	},
-	{
-		name: "salmon",
-		vegetarian: false,
-		glutenFree: true,
-		organic: false,
-		foodGroup: [0,0,0,1],
-		image: "https://upload.wikimedia.org/wikipedia/commons/c/c7/Atlantic_Salmon.png",
-		price: 12.99
-    }
+	}
 ];
 
+function compare( a, b ) {
+	if ( a.price < b.price ){
+	  return -1;
+	}
+	if ( a.price > b.price ){
+	  return 1;
+	}
+	return 0;
+  }
+  
+  products.sort(compare);
 
-
-
-
-
-
-
-
-function restrictListProducts(prods, restriction) {
-
+  function restrictListProducts(prods, restriction, category) {
 	let product_names = [];
 
 	for (let i=0; i<prods.length; i+=1) {
-
-		if ((restriction == "Vegetarian & GlutenFree & Organic") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true) && (prods[i].organic == true)){
-
-			product_names.push(prods[i]);
-
+		if((prods[i].category == category) || (category == "all")){
+			if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
+				product_names.push(prods[i].name);
+			}
+			else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
+				product_names.push(prods[i].name);
+			}
+			else if ((restriction == "Organic") && (prods[i].organic == true)){
+				product_names.push(prods[i].name);
+			}
+			else if ((restriction == "VegetarianGlutenFree") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true)){
+				product_names.push(prods[i].name);
+			}
+			else if ((restriction == "GlutenFreeOrganic") && (prods[i].glutenFree == true) && (prods[i].organic == true)){
+				product_names.push(prods[i].name);
+			}
+			else if ((restriction == "VegetarianOrganic") && (prods[i].vegetarian == true) && (prods[i].organic == true)){
+				product_names.push(prods[i].name);
+			}
+			else if ((restriction == "All") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true) && (prods[i].organic == true)){
+				product_names.push(prods[i].name);
+			}
+			else if (restriction == "None"){
+				product_names.push(prods[i].name);
+			}
 		}
-
-		else if ((restriction == "Vegetarian & GlutenFree") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true)){
-
-			product_names.push(prods[i]);
-
-		}
-
-		else if ((restriction == "Organic & GlutenFree") && (prods[i].organic == true) && (prods[i].glutenFree == true)){
-
-			product_names.push(prods[i]);
-
-		}
-
-		else if ((restriction == "Organic & Vegetarian") && (prods[i].organic == true) && (prods[i].vegetarian == true)){
-
-			product_names.push(prods[i]);
-
-		}
-
-		else if ((restriction == "Organic") && (prods[i].organic == true)){
-
-			product_names.push(prods[i]);
-
-		}
-
-		else if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-
-			product_names.push(prods[i]);
-
-		}
-
-		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
-
-			product_names.push(prods[i]);
-
-		}
-
-		else if (restriction == "None"){
-
-			product_names.push(prods[i]);
-
-		}
-
 	}
-
 	return product_names;
+}
 
+function restrictListPrices(prods, restriction, category) {
+	let prices = [];
+
+	for (let i=0; i<prods.length; i+=1) {
+		if((prods[i].category == category) || (category == "all")){
+			if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
+				prices.push(prods[i].price);
+			}
+			else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
+				prices.push(prods[i].price);
+			}
+			else if ((restriction == "Organic") && (prods[i].organic == true)){
+				prices.push(prods[i].price);
+			}
+			else if ((restriction == "VegetarianGlutenFree") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true)){
+				prices.push(prods[i].price);
+			}
+			else if ((restriction == "GlutenFreeOrganic") && (prods[i].glutenFree == true) && (prods[i].organic == true)){
+				prices.push(prods[i].price);
+			}
+			else if ((restriction == "VegetarianOrganic") && (prods[i].vegetarian == true) && (prods[i].organic == true)){
+				prices.push(prods[i].price);
+			}
+			else if ((restriction == "All") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true) && (prods[i].organic == true)){
+				prices.push(prods[i].price);
+			}
+			else if (restriction == "None"){
+				prices.push(prods[i].price);
+			}
+		}
+	}
+	return prices;
+}
+
+function restrictListImages(prods, restriction, category) {
+	let images = [];
+
+	for (let i=0; i<prods.length; i+=1) {
+		if((prods[i].category == category) || (category == "all")){
+			if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
+				images.push(prods[i].image);
+			}
+			else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
+				images.push(prods[i].image);
+			}
+			else if ((restriction == "Organic") && (prods[i].organic == true)){
+				images.push(prods[i].image);
+			}
+			else if ((restriction == "VegetarianGlutenFree") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true)){
+				images.push(prods[i].image);
+			}
+			else if ((restriction == "GlutenFreeOrganic") && (prods[i].glutenFree == true) && (prods[i].organic == true)){
+				images.push(prods[i].image);
+			}
+			else if ((restriction == "VegetarianOrganic") && (prods[i].vegetarian == true) && (prods[i].organic == true)){
+				images.push(prods[i].image);
+			}
+			else if ((restriction == "All") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true) && (prods[i].organic == true)){
+				images.push(prods[i].image);
+			}
+			else if (restriction == "None"){
+				images.push(prods[i].image);
+			}
+		}
+	}
+	return images;
 }
 
 
 
 function getTotalPrice(chosenProducts) {
-
 	totalPrice = 0;
-
 	for (let i=0; i<products.length; i+=1) {
-
 		if (chosenProducts.indexOf(products[i].name) > -1){
-
 			totalPrice += products[i].price;
-
 		}
-
 	}
-
 	return totalPrice;
-
 }
--- lab3/scripts/main.js
+++ lab3/scripts/main.js
@@ -1,368 +1,103 @@
 function openInfo(evt, navName) {
 
-
-
-
 	navcontent = document.getElementsByClassName("navcontent");
-
 	for (i = 0; i < navcontent.length; i++) {
-
 		navcontent[i].style.display = "none";
-
 	}
-
-
-
-
 
 	navlinks = document.getElementsByClassName("navlinks");
-
 	for (i = 0; i < navlinks.length; i++) {
-
 		navlinks[i].className = navlinks[i].className.replace(" active", "");
-
 	}
-
-
-
 
 	document.getElementById(navName).style.display = "block";
-
 	evt.currentTarget.className += " active";
-
-
-
 }
 
-
-
-
-
-
-
-var acc = document.getElementsByClassName("accordion");
-
-var i;
-
-
-
-for (i = 0; i < acc.length; i++) {
-
-  acc[i].addEventListener("click", function() {
-
-
-    this.classList.toggle("active");
-
-
-
-
-
-    var panel = this.nextElementSibling;
-
-    if (panel.style.display === "block") {
-
-      panel.style.display = "none";
-
-    } else {
-
-      panel.style.display = "block";
-
-    }
-
-  });
-
-}
-
-
-
-var bcc = document.getElementsByClassName("panel");
-
-
-
-function toggleItem() {
-
-	for (i = 0; i < acc.length; i++) {
-
-		if(acc[i].className === "accordion active") {
-
-			acc[i].className = "accordion";
-
-			bcc[i].style.display = "none";
-
-		}
-
-	}
-
-}
-
-
-
-
-
-
-
-function prefFunction() {
-
-	var x = document.getElementById("Vegetarian").checked;
-
-	var y = document.getElementById("Organic").checked;
-
-	var z = document.getElementById("GlutenFree").checked;
-
-
-
-	if(x == true && y == true && z == true) {
-
-		populateListProductChoices("Vegetarian & GlutenFree & Organic", 'displayFruit', 'displayGrain', 'displayMeat', 'displaySeafood')
-
-	}
-
-	else if (x == true && y == true) {
-
-		populateListProductChoices("Organic & Vegetarian", 'displayFruit', 'displayGrain', 'displayMeat', 'displaySeafood')
-
-	}
-
-	else if (x == true && z ==true) {
-
-		populateListProductChoices("Vegetarian & GlutenFree", 'displayFruit', 'displayGrain', 'displayMeat', 'displaySeafood')
-
-	}
-
-	else if (y == true && z == true) {
-
-		populateListProductChoices("Organic & GlutenFree", 'displayFruit', 'displayGrain', 'displayMeat', 'displaySeafood')
-
-	}
-
-	else if (x == true) {
-
-		populateListProductChoices("Vegetarian", 'displayFruit', 'displayGrain', 'displayMeat', 'displaySeafood')
-
-	}
-
-	else if (y == true) {
-
-		populateListProductChoices("Organic", 'displayFruit', 'displayGrain', 'displayMeat', 'displaySeafood')
-
-	}
-
-	else if (z == true) {
-
-		populateListProductChoices("GlutenFree", 'displayFruit', 'displayGrain', 'displayMeat', 'displaySeafood')
-
-	}
-
-	else {
-
-		populateListProductChoices("None", 'displayFruit', 'displayGrain', 'displayMeat', 'displaySeafood')
-
-	}
-
-}
-
-
-
-
-prefFunction()
-
-
-
-
-
-
-function populateListProductChoices(slct1, slct2, slct3, slct4, slct5) {
-
-    var s1 = slct1;
-
-    var s2 = document.getElementById(slct2);
-
-		var s3 = document.getElementById(slct3);
-
-		var s4 = document.getElementById(slct4);
-
-		var s5 = document.getElementById(slct5);
-
-
-
-    s2.innerHTML = "";
-
-		s3.innerHTML = "";
-
-		s4.innerHTML = "";
-
-		s5.innerHTML = "";
-
-
-
-
-
-    var optionArray = restrictListProducts(products, s1);
-
-
-
-		// Line below adapted from https://www.w3schools.com/js/tryit.asp?filename=tryjs_array_sort_object1
-
-		optionArray.sort(function(a,b) {return a.price - b.price});
-
-
-
-
-
-
-
-	for (i = 0; i < optionArray.length; i++) {
-
-
-
-		var productName = optionArray[i];
-
-		var image = productName.img;
-
-
-
+function populateListProductChoices(slct1, slct2, category) {
+    var s2 = document.getElementById(slct2);	
+    s2.innerHTML = "";		
+    var optionArray = restrictListProducts(products, slct1, category);
+	var priceArray = restrictListPrices(products, slct1, category);
+	var imageArray = restrictListImages(products, slct1, category);
+	
+	for (i = 0; i < optionArray.length; i++) {			
+		var productName = optionArray[i];		
 		var checkbox = document.createElement("input");
-
 		checkbox.type = "checkbox";
-
 		checkbox.name = "product";
-
-		checkbox.value = productName.name;
-
-
-
-		var picture = document.createElement("IMG");
-
-		picture.setAttribute("src", image);
-
-		picture.setAttribute("width", "250");
-
- 		picture.setAttribute("height", "250");
-
-
-
+		checkbox.value = productName;
+		s2.appendChild(checkbox);
+		
 		var label = document.createElement('label')
-
-		label.htmlFor = productName.name;
-
-		label.appendChild(document.createTextNode(productName.name + " " + productName.price));
-
-
-
-		if(productName.foodGroup[0] == 1) {
-
-			s2.appendChild(checkbox);
-
-			s2.appendChild(picture);
-
-			s2.appendChild(document.createElement("br"));
-
-			s2.appendChild(checkbox);
-
-			s2.appendChild(label);
-
-			s2.appendChild(document.createElement("br"));
-
-		}
-
-		else if(productName.foodGroup[1] == 1) {
-
-			s3.appendChild(checkbox);
-
-			s3.appendChild(picture);
-
-			s3.appendChild(document.createElement("br"));
-
-			s3.appendChild(checkbox);
-
-			s3.appendChild(label);
-
-			s3.appendChild(document.createElement("br"));
-
-		}
-
-		else if(productName.foodGroup[2] == 1) {
-
-			s4.appendChild(checkbox);
-
-			s4.appendChild(picture);
-
-			s4.appendChild(document.createElement("br"));
-
-			s4.appendChild(checkbox);
-
-			s4.appendChild(label);
-
-			s4.appendChild(document.createElement("br"));
-
-		}
-
-		else {
-
-			s5.appendChild(checkbox);
-
-			s5.appendChild(picture);
-
-			s5.appendChild(document.createElement("br"));
-
-			s5.appendChild(checkbox);
-
-			s5.appendChild(label);
-
-			s5.appendChild(document.createElement("br"));
-
-		}
-
+		label.htmlFor = productName;
+		label.appendChild(document.createTextNode(productName + " ($" + priceArray[i] + ")"));
+		s2.appendChild(label);
+		
+		s2.appendChild(document.createElement("br"));   
+		var img = document.createElement('img');
+		img.src = imageArray[i];
+		img.setAttribute("width","200");
+		img.setAttribute("height","200");
+		s2.appendChild(img);
+		
+		s2.appendChild(document.createElement("br"));    
 	}
-
+	s2.appendChild(document.createTextNode("-----End of results-----"));
+}
+	
+function selectedItems(){	
+	var ele = document.getElementsByName("product");
+	var chosenProducts = [];	
+	var c = document.getElementById('displayCart');
+	c.innerHTML = "";	
+	
+	var para = document.createElement("P");
+	para.innerHTML = "You selected : ";
+	para.appendChild(document.createElement("br"));
+	for (i = 0; i < ele.length; i++) { 
+		if (ele[i].checked) {
+			para.appendChild(document.createTextNode(ele[i].value));
+			para.appendChild(document.createElement("br"));
+			chosenProducts.push(ele[i].value);
+		}
+	}
+		
+	c.appendChild(para);
+	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts)));
+	openInfo(event, 'Cart');
 }
 
-
-
-function selectedItems(){
-
-
-
-	var ele = document.getElementsByName("product");
-
-	var chosenProducts = [];
-
-
-
-	var c = document.getElementById('displayCart');
-
-	c.innerHTML = "";
-
-
-
-
-	var para = document.createElement("P");
-
-	para.innerHTML = "You selected : ";
-
-	para.appendChild(document.createElement("br"));
-
-	for (i = 0; i < ele.length; i++) {
-
-		if (ele[i].checked) {
-
-			para.appendChild(document.createTextNode(ele[i].value));
-
-			para.appendChild(document.createElement("br"));
-
-			chosenProducts.push(ele[i].value);
-
-		}
-
+function applyFilters(){
+	var category = document.getElementById("categorySelect").value;
+	var x = document.getElementById('filter1');
+	var y = document.getElementById('filter2');
+	var z = document.getElementById('filter3');
+	var slct1 = "None";	
+	
+	if(!x.checked && !y.checked && !z.checked){
+		slct1 = "None";
+	}else if(!x.checked && !y.checked && z.checked){
+		slct1 = "Organic";
+	}else if(!x.checked && y.checked && !z.checked){
+		slct1 = "GlutenFree";
+	}else if(!x.checked && y.checked && z.checked){
+		slct1 = "GlutenFreeOrganic";
+	}else if(x.checked && !y.checked && !z.checked){
+		slct1 = "Vegetarian";
+	}else if(x.checked && !y.checked && z.checked){
+		slct1 = "VegetarianOrganic";
+	}else if(x.checked && y.checked && !z.checked){
+		slct1 = "VegetarianGlutenFree";
+	}else if(x.checked && y.checked && z.checked){
+		slct1 = "All";
 	}
+	populateListProductChoices(slct1, 'displayProduct', category);
+}
 
-
-
-
-	c.appendChild(para);
-
-	c.appendChild(document.createTextNode("Total Price is " + getTotalPrice(chosenProducts).toFixed(2)));
-
-
-
+function start(){
+	var category = document.getElementById("categorySelect").selectedIndex;
+	document.getElementById("categorySelect").selectedIndex = category;
+	applyFilters();
+	openInfo(event, 'Products');
 }
